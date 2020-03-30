1cb4b3ee1d2d966dbdf6eaae87e0dc04a82120c5
antonkarpyza <antonalexk@gmail.com>
2020-03-30 23:46:24 +0300

    week2

--- src/Week2/Task1.java
+++ src/Week2/Task1.java
@@ -5,6 +5,7 @@ public class Task1 {
     public static void main(String[] args) {
 
         System.out.println("Таблица умножения");
+
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
                 System.out.print(i*j + " ");
